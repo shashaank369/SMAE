1d876554dcce484e122a5911a51bc798520a5fa7
shashaank369 <11239a005@kanchiuniv.ac.in>
2026-06-28 16:00:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const DB_PATH = process.env.DB_PATH || './smae-contacts.db';
-const RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY || 'YOUR_RECAPTCHA_SECRET_KEY';
+const RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY || '6LdEFDotAAAAAKQxzt23KxLNqanTRRc9p85X2EAc';
 const RECAPTCHA_ENABLED = RECAPTCHA_SECRET_KEY && !RECAPTCHA_SECRET_KEY.includes('YOUR_RECAPTCHA_SECRET_KEY');
 
 // Middleware
